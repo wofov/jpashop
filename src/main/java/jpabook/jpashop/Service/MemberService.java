c15e4c3c284179e925ef7ed9aa0209bd295ea0c0
wofov <hys950526@naver.com>
2024-02-29 21:59:37 +0900

    shop service 구축

---
+++ src/main/java/jpabook/jpashop/Service/MemberService.java
@@ -0,0 +1,43 @@
+package jpabook.jpashop.Service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor//private final만 생성자 생성함.
+public class MemberService {
+    // lombok 만들기 -> 커맨드 + N
+    // 테스트 만들기 -> 커맨드 + 쉬프트 + T
+    private final MemberRepository memberRepository;
+
+
+    @Transactional
+    public Long join(Member member) {
+        validateDuplicateMember(member); //중복 회원 검증 memberRepository.save(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+
+
+}
