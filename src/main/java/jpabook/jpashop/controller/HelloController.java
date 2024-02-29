4acbf9c1d9c0e970bd23cdec8ff0326792490562
wofov <hys950526@naver.com>
2024-02-29 21:59:58 +0900

    단축키 주석

--- src/main/java/jpabook/jpashop/controller/HelloController.java
+++ src/main/java/jpabook/jpashop/controller/HelloController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
+
+    //전체 검색 -> 커맨드 + 쉬프트 + F
+    //옵션 + 엔터 -> 코드 간단 수정
     @GetMapping("/hello")
     public String hello(Model model){
         model.addAttribute("data","hello");
