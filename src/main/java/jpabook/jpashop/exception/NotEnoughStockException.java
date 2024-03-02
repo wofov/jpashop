670f9bf0ca0715363511cce47a53ccc2f3fbbf51
wofov <hys950526@naver.com>
2024-03-02 22:44:01 +0900

    -

--- src/main/java/jpabook/jpashop/exception/NotEnoughStockException.java
+++ src/main/java/jpabook/jpashop/exception/NotEnoughStockException.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.exception;
 
 public class NotEnoughStockException extends RuntimeException{
+
     public NotEnoughStockException() {
     }
     public NotEnoughStockException(String message) {
