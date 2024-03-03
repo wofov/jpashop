ed36f929e5421063c010b7091841626a606d8ca9
wofov <hys950526@naver.com>
2024-03-03 14:53:20 +0900

    -

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -9,4 +9,11 @@ public class Address {
     private String city;
     private String street;
     private String zipCode;
+    protected Address() {
+    }
+    public Address(String city, String street, String zipCode) {
+        this.city = city;
+        this.street = street;
+        this.zipCode = zipCode;
+    }
 }
