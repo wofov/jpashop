4ae1499d32ee8ff86b842e8ae5a832f8bc96721e
wofov <hys950526@naver.com>
2024-03-22 12:15:26 +0900

    주문 조회 v4 dto

---
+++ src/main/java/jpabook/jpashop/domain/OrderSimpleQueryDto.java
@@ -0,0 +1,22 @@
+package jpabook.jpashop.domain;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+@Data
+public class OrderSimpleQueryDto {
+
+    private Long orderId;
+    private String name;
+    private LocalDateTime orderDate; //주문시간
+    private OrderStatus orderStatus;
+    private Address address;
+    public OrderSimpleQueryDto(Order order) {
+        orderId = order.getId();
+        name = order.getMember().getName();
+        orderDate = order.getOrderDate();
+        orderStatus = order.getStatus();
+        address = order.getDelivery().getAddress();
+    }
+
+}
