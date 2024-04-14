4ee477879841a1f728cf75970f81c91d49865ba7
wofov <hys950526@naver.com>
2024-04-14 16:24:01 +0900

    aop --

---
+++ src/main/java/jpabook/jpashop/aop/TimeTraceAop.java
@@ -0,0 +1,26 @@
+package jpabook.jpashop.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component//빈 등록 위해
+public class TimeTraceAop {
+
+    @Around("execution(* jpabook.jpashop..*(..))")
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        System.out.println("----");
+        long start = System.currentTimeMillis();
+        System.out.println("START: " + joinPoint.toString());
+        try {
+            return joinPoint.proceed();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("END: " + joinPoint.toString() + " " + timeMs + "ms");
+        }
+    }
+}
